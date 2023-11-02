9a016535f64810053dbc3570112c6c9b42dc073e
MuhireIghor <muhireighor123@gmail.com>
2023-11-02 08:11:58 +0200

    [feat]:Added sockets

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const {Server} = require ("socket.io");
+const {createServer} = require("http");
 const cors = require("cors")
 const routes = require("./router")
 const app = express();
 app.use(express.json());
+const server = createServer();
+const io = new Server(server);
+
 const PORT = process.env.port|4000;
 app.use(cors({
     origin: "*"
@@ -15,7 +20,7 @@ app.get("/api",(req,res)=>{
         message:"Welcome to Health Pro Platform"
     })
 })
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`);
 })
 module.exports = app
